44272eac345f60b9a77e9e67eef1f2bc155a5bbf
Rundll86 <17784453279@163.com>
2024-06-22 07:19:34 +0800

    update

--- src/AutoVM.ts
+++ src/AutoVM.ts
@@ -1,12 +1,11 @@
 import { exposeToMainWorld } from "./Common";
-import { PresetVM, ValueType } from "./DataStruct";
+import { ValueType } from "./DataStruct";
 import { createUnit, readUnit } from "./Unit";
-var AutoVM: PresetVM = {
+exposeToMainWorld({
     time: createUnit(["h", "m", "s"], ValueType.NUMBER, 60),
     distance: createUnit(["km", "hm", "tm", "m", "dm", "cm", "mm", "um", "nm"], ValueType.NUMBER, 10),
     velocity: createUnit("auto", ValueType.RATIO, {
         parent: readUnit("time"),
         child: readUnit("velocity")
     })
-};;
-exposeToMainWorld(AutoVM, "AutoVM");
+}, "AutoVM");
